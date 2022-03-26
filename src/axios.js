3098cc1a3df7a70c53a858f4ab4e46d538ac2c57
Magatte <magatte@inbox.ru>
2022-03-26 16:37:45 +0500

    axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//onst baseURL = 'https://verysoongaalguimoney.herokuapp.com/api/';
-const baseURL ='http://127.0.0.1:8000/api/'
+const baseURL = 'https://gaalguimoneyback.herokuapp.com/api/';
+//const baseURL ='http://127.0.0.1:8000/api/'
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
